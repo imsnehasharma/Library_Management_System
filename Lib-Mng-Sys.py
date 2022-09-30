c48faf13e70a6940606f592d45e3f3920ce5a0e3
Lalit <lalitkumarsharma.jsr@gmail.com>
2022-09-30 11:25:57 +0530

    searching

--- Lib-Mng-Sys.py
+++ Lib-Mng-Sys.py
@@ -1,5 +1,34 @@
-from http.cookies import BaseCookie
-from tkinter import N
+#import mysql.connector as mys
+#mycon = mys.connect(host='localhost',user='root',password='lms')
+#mycur = mycon.cursor()
+
+
+def search_book():
+  a = input('Enter Book ID or Book Name : ')
+  if a.isnumeric():
+    query = "select * from books where id = {}".format(int(a))
+  else :
+    query = "select * from books where name = {}".format(a)
+  mycur.execute(query)
+
+def search_user():
+  a = input('Enter User ID or User Name : ')
+  if a.isnumeric():
+    query = "select * from users where id = {}".format(int(a))
+  else :
+    query = "select * from users where name = {}".format(a)
+  mycur.execute(query)
+
+def issue_book():
+  a = int(input('Enter Book ID : '))
+  b = int(input('Enter User ID : '))
+  query = "update books set issued_to = {}".format(b)
+  mycur.execute(query)
+  query = "update users set book_assigned = {}".format(a)
+
+
+
+
 
 
 print(
@@ -12,3 +41,19 @@ print(
 ' [6] Update User Records\n'
 )
 n = int(input('Enter Desired Number Option: '))
+
+while True:
+  if n == 1:
+    search_book()
+  elif n == 2:
+    search_user()
+  elif n == 3:
+    issue_book()
+  elif n == 4:
+    return_book()
+  elif n == 5:
+    update_books()
+  elif n == 6:
+    update_user()
+  else:
+    n = int(input('Please Enter a Valid Choice: '))
