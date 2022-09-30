ccab2b39d47391e05fc3ac8bc73ed5aae131dd37
Lalit <lalitkumarsharma.jsr@gmail.com>
2022-09-30 10:34:26 +0530

    entering input

---
+++ Lib-Mng-Sys.py
@@ -0,0 +1,14 @@
+from http.cookies import BaseCookie
+from tkinter import N
+
+
+print(
+'Hey, What would you like to do?\n'
+' [1] Search book from record\n'
+' [2] Search user from record\n' 
+' [3] Issue a Book\n'
+' [4] Return a Book\n'
+' [5] Update Inventory\n'
+' [6] Update User Records\n'
+)
+n = int(input('Enter Desired Number Option: '))
