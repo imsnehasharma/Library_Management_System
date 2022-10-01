a0985081176fac734ef7cd1dd69be1db1ea52e82
Lalit <lalitkumarsharma.jsr@gmail.com>
2022-10-01 14:33:10 +0530

    touching up

--- Lib-Mng-Sys.py
+++ Lib-Mng-Sys.py
@@ -8,6 +8,7 @@ def search_book():
   "#      '*' to see all books      # \n"
   "# '$' to see all available books # \n"
   "#  '$$' to see all issued books  # \n")
+  
   while True:
     if a.upper() == 'EXIT':
       break
@@ -21,8 +22,10 @@ def search_book():
       query = "select * from books where id = {}".format(int(a))
     else :
       query = "select * from books where name = '{}'".format(a)
+    
     mycur.execute(query)
     dt = mycur.fetchall()
+    
     if dt != []:
       print('-'*80)
       print('%5s'%'ID', '%30s'%'Name', '%20s'%"Author's Name", '%20s'%'Issued To' )
@@ -54,8 +57,10 @@ def search_user():
       query = "select * from users where id = {}".format(int(a))
     else :
       query = "select * from users where name = '{}'".format(a)
+    
     mycur.execute(query)
     dt = mycur.fetchall()
+    
     if dt != []:
       print('-'*80)
       print('%5s'%'ID', '%30s'%'Name', '%20s'%"Phone Number", '%20s'%'Books Borrowed' )
@@ -76,6 +81,7 @@ def issue_book():
     b = input('Enter User ID : ')
     if b.upper() == 'EXIT':
       break
+    
     query = "select id from books where issued_to IS NULL"
     mycur.execute(query)
     dt = mycur.fetchall()
@@ -107,12 +113,15 @@ def issue_book():
 
 def return_book():
   a = input('Enter Book ID : ')
+  
   while True:
     if a.upper() == 'EXIT':
       break
+    
     query = "select id from books where issued_to IS NOT NULL"
     mycur.execute(query)
     dt = mycur.fetchall()
+    
     if (int(a),) in dt:
       query = "update books set issued_to = NULL where id = {}".format(int(a))
       mycur.execute(query)
@@ -130,20 +139,24 @@ def add_books():
   query = "select Max(id) from books"
   mycur.execute(query)
   dt = mycur.fetchone()
+  
   while True:
     nm = input('Enter Book Name : ').upper()
     if nm.upper() == 'EXIT':
       break
-    an = input('Enter Author Name : ').capwords()
+    an = input('Enter Author Name : ').title()
     if an.upper() == 'EXIT':
       break
+    
     query = "insert into books (name,author_name) values('{}','{}')".format(nm,an)
     mycur.execute(query)
     mycon.commit()
+    print('.'*85)
+  
   query = "select * from books where id > {}".format(dt[0])
   mycur.execute(query)
   dt = mycur.fetchall()
-  print('Following Books have been added')
+  print('\n Following Books have been added')
   print('-'*80)
   print('%5s'%'ID', '%30s'%'Name', '%20s'%"Author's Name", '%20s'%'Issued To' )
   print('-'*80)
@@ -152,12 +165,15 @@ def add_books():
 
 def del_books():
   while True:
+    
     a = input('Enter ID of the Book to be Deleted : ')
     if a.upper() == 'EXIT':
       break
+    
     query = "select id from books"
     mycur.execute(query)
     dt = mycur.fetchall()
+    
     if a.isnumeric():
       if (int(a),) in dt:
         query = "delete from books where id = {}".format(int(a))
@@ -176,8 +192,10 @@ def mod_books():
   query = "select id from books"
   mycur.execute(query)
   dt = mycur.fetchall()
+  
   while True:
     a = input('Enter Book ID : ')
+    
     while True:
       if a.upper() == 'EXIT':
         return
@@ -268,22 +286,29 @@ def add_users():
   query = "select Max(id) from users where id is not null"
   mycur.execute(query)
   dt = mycur.fetchone()
+  
   while True:
+    
     nm = input('Enter User Name : ').capitalize()
     if nm.upper() == 'EXIT':
       break
-      x = 'EXIT'
+    
     pn = input('Enter Phone Number : ')
     if pn.upper() == 'EXIT':
       break
-      x = 'EXIT'
-    query = "insert into users (name,phone_no) values('{}','{}')".format(nm,pn)
-    mycur.execute(query)
-    mycon.commit()
+    
+    if pn.isnumeric() and len(pn) == 10:
+      query = "insert into users (name,phone_no) values('{}','{}')".format(nm,pn)
+      mycur.execute(query)
+      mycon.commit()
+    else:
+      print('Please Enter a Valid Phone Number.')
+    print('.'*85)
+  
   query = "select * from users where id > {}".format(dt[0])
   mycur.execute(query)
   dt = mycur.fetchall()
-  print('Following Users have been added')
+  print('\n Following Users have been added')
   print('-'*80)
   print('%5s'%'ID', '%30s'%'Name', '%20s'%"Phone Number", '%20s'%'Books Borrowed' )
   print('-'*80)
@@ -295,45 +320,73 @@ def del_users():
     a = input('Enter ID of the User to be Deleted : ')
     if a.upper() == 'EXIT':
       break
-    else :
+   
+    if a.isnumeric():
       a = int(a)
-    query = "select id from users"
-    mycur.execute(query)
-    dt = mycur.fetchall()
-    if (a,) in dt :
-      query = "delete from users where id = {}".format(a)
+      query = "select id from users"
       mycur.execute(query)
-      mycon.commit()
-      print('User of the Given ID has been removed from the User List. \n')
-    else:
-      print('There is no such User.')
-    print('.'*85)
+      dt = mycur.fetchall()
+      if (a,) in dt :
+        query = "delete from users where id = {}".format(a)
+        mycur.execute(query)
+        mycon.commit()
+        print('User of the Given ID has been removed from the User List. \n')
+      else:
+        print('There is no such User.')
+      print('.'*85)
+    else :
+      print("Please Enter ID or Type 'EXIT' to reach the Main Menu.")
+      print('.'*85)
 
 def mod_users():
+  query = "select id from users"
+  mycur.execute(query)
+  dt = mycur.fetchall()
+ 
   while True:
     a = input('Enter User ID : ')
-    if a.upper() == 'EXIT':
-      break
-    else:
-      a = int(a)
+   
+    while True:
+      if a.upper() == 'EXIT':
+        return
+      elif a.isnumeric():
+        if (int(a),) in dt:
+          a = int(a)
+          break
+        else:
+          print('.'*85)
+          a = input('Please Enter a Valid ID : ')       
+      else:
+        print('.'*85)
+        a = input('Please Enter a Valid ID : ')
+
     b = input('What would you like to change? \n'
     '   [1] Name \n'
     '   [2] Phone Number \n')
-    if b.upper() == 'EXIT':
-      break
-    else:
-      b = int(b)
-      
-    if b == 1:
-      nm = input('Enter New Name : ')
-      query = "update users set name = '{}' where id = {}".format(nm,a)
-      break
-    elif b == 2:
-      an = input('Enter New Phone Number : ')
-      query = "update users set phone_no = '{}' where id = {}".format(an,a)
-      break
-    else :
-      b = input('Please Enter a Valid Choice : ')
+    
+    while True:
+      if b.upper() == 'EXIT':
+        return
+      elif b.isnumeric():
+        b = int(b)
+        if b == 1:
+          nm = input('Enter New Name : ')
+          if nm.upper() == 'EXIT':
+            return
+          query = "update users set name = '{}' where id = {}".format(nm,a)
+          break
+        elif b == 2:
+          an = input('Enter New Phone Number : ')
+          if an.upper() == 'EXIT':
+            return
+          query = "update users set phone_no = '{}' where id = {}".format(an,a)
+          break  
+        else:
+          print('.'*85)
+          b = input('Please Enter a Valid Choice : ')
+      else:
+          print('.'*85)
+          b = input('Please Enter a Valid Choice : ')
       
     mycur.execute(query)
     mycon.commit()
@@ -351,6 +404,7 @@ def update_user():
   '  [2] Delete User \n'
   '  [3] Modify Details of User \n')
   x = input('Enter Desired Number Option : ')
+  
   while True:
     if x.upper() == 'EXIT':
       break
@@ -377,45 +431,52 @@ def update_user():
       print('.'*85)
       x = input('Please Enter a Valid Choice : ')
 
-
 def main_menu(n):
+  
   while True:
+    
     if n == 1:
       print('='*150)
       print(' '*65, 'BOOK SEARCH MENU \n')
       search_book()
       print('='*150)
       break
+    
     elif n == 2:
       print('='*150)
       print(' '*65, 'USER SEARCH MENU \n')
       search_user()
       print('='*150)
       break
+    
     elif n == 3:
       print('='*150)
       print(' '*65, 'ISSUE BOOK MENU \n')
       issue_book()
       print('='*150)
       break
+    
     elif n == 4:
       print('='*150)
       print(' '*65, 'RETURN BOOK MENU \n')
       return_book()
       print('='*150)
       break
+    
     elif n == 5:
       print('='*150)
       print(' '*65, 'UPDATE INVENTORY MENU \n')
       update_books()
       print('='*150)
       break
+   
     elif n == 6:
       print('='*150)
       print(' '*65, 'UPDATE USER LIST MENU \n')
       update_user()
       print('='*150)
       break
+    
     else:
       n = int(input('Please Enter a Valid Choice: '))
 
@@ -434,11 +495,10 @@ while True:
   )
 
   print(' '*50, "** Type 'EXIT' at any point to Return to Main Menu ** \n")
- # try:
   n = input('Enter Desired Number Option: ')
-  main_menu(n)
- # except:
-  #  n = input('Please Enter a Valid Choice : ')
-   # main_menu(n)
-
+  if n.isnumeric():
+    n = int(n)
+    main_menu(n)
+  else:
+    print('Please Enter Valid Option.')
  
